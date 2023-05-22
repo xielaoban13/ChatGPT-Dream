a90bcce813480a470a12c79ee13e35e935d66d35
xielaoban13 <129215613+xielaoban13@users.noreply.github.com>
2023-05-22 17:16:17 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -325,7 +325,7 @@ export function ImagePreviewer(props: {
           <div>
             <><div id="main-title" className={styles["main-title"]}>ChatGPT Dream</div>
             <div id="sub-title" className={styles["sub-title"]}></div>
-             <script src="https://v1.hitokoto.cn/?encode=js&select=%23main-title" defer></script></>
+             <script src="https://v1.hitokoto.cn/?encode=js&select=%23sub-title" defer></script></>
 
             <div className={styles["icons"]}>
               <Avatar avatar={config.avatar}></Avatar>
